0fcd87a561c0f8ba3647a13789b652e098d52a15
Julio Feblez <chano.feblez@yahoo.com>
2024-09-15 18:02:55 -0700

    CORS Configured-1

--- src/main/java/com/nailsSalon/AdriDesign/security/config/WebConfig.java
+++ src/main/java/com/nailsSalon/AdriDesign/security/config/WebConfig.java
@@ -12,14 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(
-                        "http://localhost:8100",       // Desarrollo local con Ionic
-                        "http://localhost:4200",       // Desarrollo local con Angular
-                        "https://adrinailsdesign-c393e5baf34a.herokuapp.com",  // Tu aplicación en Heroku
-                        "capacitor://localhost"        // Para accesos desde dispositivos móviles usando Capacitor
-                )
+                .allowedOrigins("*")  // Permitir todas las solicitudes desde cualquier origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);  // Puedes cambiar a true si manejas cookies
     }
 }
